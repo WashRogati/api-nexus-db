d8101f31ecc972099e1612dcd63a4e7a0118a2a2
Wellington Rogati <wellingtonrogati@gmail.com>
2023-11-26 11:57:43 -0300

    ajustes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,16 +4,29 @@ import dbInstance, { Database } from '../db/database';
 const router = Router();
 
 router.get('/minharotaget', async (req, res) => {
-  const profissionais = await dbInstance.query('SELECT * FROM profissionais;');
-  res.json({
-    data: profissionais[0],
-  });
+  try {
+    const profissionais = await dbInstance.query(
+      'SELECT * FROM profissionais;',
+    );
+    res.json({
+      data: profissionais[0],
+    });
+  } catch (e: any) {
+    console.log('err: ', e.message);
+    res.status(400).json({
+      message: 'Erro ao listar dados',
+    });
+  }
 });
 
 router.post('/minharotapost', async (req, res) => {
   try {
-    const { name } = req.body;
-    await dbInstance.insertOne('profissionais', { name: name });
+    const { name, age, last_name } = req.body;
+    await dbInstance.insertOne('profissionais', {
+      name: name,
+      age: age,
+      last_name: last_name,
+    });
     res.json({ message: 'Registro inserido' });
   } catch (e: any) {
     console.log('err: ', e.message);
